1ab1212429341184cdd639323932c2c7e336b164
Felix Henninger <mailbox@felixhenninger.com>
2019-07-09 13:21:25 +0200

    Add basic bootstrap styling

--- src/App.js
+++ src/App.js
@@ -2,27 +2,36 @@ import React from 'react'
 import Dropzone from 'react-dropzone'
 
 import './App.css'
+import { Container, Row, Col,
+  Card, CardBody, CardTitle, CardText } from 'reactstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Psych-DS validator</h1>
-      </header>
-      <main>
+const App = () =>
+  <Container className="app-container">
+    <Row>
+      <Col className="text-center py-4">
+        <h1 className="h2">Psych-DS validator prototype</h1>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
         <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
           {({getRootProps, getInputProps}) => (
-            <section>
-              <div {...getRootProps()}>
+            <Card className="text-center py-3">
+              <CardBody {...getRootProps()}>
                 <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
-              </div>
-            </section>
+                <CardTitle className="h4">
+                  Start validation
+                </CardTitle>
+                <CardText>
+                  Please drag and drop your Psych-DS folder here,
+                  or click to select it
+                </CardText>
+              </CardBody>
+            </Card>
           )}
         </Dropzone>
-      </main>
-    </div>
-  );
-}
+      </Col>
+    </Row>
+  </Container>
 
 export default App;

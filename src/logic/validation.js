993205763ca2f01e7f0c47d3548b0a50ba9921ce
Felix Henninger <mailbox@felixhenninger.com>
2019-07-09 15:20:05 +0200

    Check filenames for non-alphanumeric (plus underscore and minus) characters

--- src/logic/validation.js
+++ src/logic/validation.js
@@ -1,7 +1,26 @@
-const validate = (files) => {
-  const errors = []
-  console.log(files)
-  return errors
-}
+const filenames_ascii = files =>
+  // Check whether the file names are alphanumeric.
+  // If the file path matches, everything is ok (return undefined),
+  // otherwise return an error including further data
+  files.map(
+    f => /^[a-zA-Z0-9_\-/.]+$/g.test(f.path)
+      ? undefined
+      : {
+          message: 'Non-alphanumeric file path',
+          file: f.path,
+          severity: 'error',
+        }
+  )
+
+const checks = [
+  filenames_ascii,
+]
+
+const validate = (files) =>
+  // Run all of the checks for all of the files,
+  // then filter non-errors (undefined)
+  checks
+    .flatMap(check => check(files))
+    .filter(e => e !== undefined)
 
 export default validate

e4d14ebf9921b4aaf8e5d66ba14ff6111fd9406d
Felix Henninger <mailbox@felixhenninger.com>
2019-09-01 18:51:53 +0200

    Move filename checks to new wrapper

--- validator-core/src/checks/filenames.js
+++ validator-core/src/checks/filenames.js
@@ -1,13 +1,18 @@
-export const filenames_alphanumeric = (files) =>
+import { makeCheck } from '../util'
+
+const file_name_alphanumeric = ([path, _]) =>
   // Check whether the file names are alphanumeric.
   // If the file path matches, everything is ok (return undefined),
   // otherwise return an error including further data
-  Object.keys(files).map(
-    path => /^[a-zA-Z0-9_\-/.]+$/g.test(path)
-      ? undefined
-      : {
-          message: 'Non-alphanumeric file path',
-          file: path,
-          severity: 'error',
-        }
-  )
+  /^[a-zA-Z0-9_\-/.]+$/g.test(path)
+    ? undefined
+    : {
+        message: 'Non-alphanumeric file path',
+        file: path,
+        severity: 'error',
+      }
+
+export const filenames_alphanumeric = makeCheck(file_name_alphanumeric, {
+  glob: '**/*.*',
+  mode: 'per_file'
+})

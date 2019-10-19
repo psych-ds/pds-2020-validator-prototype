687981ab63c84033b1840baae0f4fbc7ec8963de
Felix Henninger <mailbox@felixhenninger.com>
2019-10-19 21:34:32 +0200

    Add preliminary header

--- validator-web/src/components/Header/index.js
+++ validator-web/src/components/Header/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Nav, NavItem, NavLink } from 'reactstrap'
 
 export default () =>
   <>
     <h1 className="h2">Psych-DS validator prototype</h1>
+    <Nav className="justify-content-center">
+      <NavItem>
+        <NavLink href="https://psych-ds.github.io/">
+          Psych-DS project
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://docs.google.com/document/d/1u8o5jnWk0Iqp_J06PTu5NjBfVsdoPbBhstht6W0fFp0/edit">
+          Specification
+        </NavLink>
+      </NavItem>
+    </Nav>
     <p
       className="mx-md-auto"
       style={{
